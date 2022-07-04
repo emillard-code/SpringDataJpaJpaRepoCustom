00ac0570cf2c3a8e366cb379d84eccf0e3973efc
Emerson Millard <emillard.code@gmail.com>
2022-07-03 17:10:45 -0700

    Converted from CrudRepository to JpaRepository.

--- src/main/java/com/project/repository/CoderRepository.java
+++ src/main/java/com/project/repository/CoderRepository.java
@@ -1,12 +1,12 @@
 package com.project.repository;
 
 import com.project.model.Coder;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface CoderRepository extends CrudRepository<Coder, Integer> {
+public interface CoderRepository extends JpaRepository<Coder, Integer> {
 
     // Spring Data JPA is able to automatically add functionality to certain methods.
     // If you use findBy<FieldName> method format, it will return all entries with matching data.
